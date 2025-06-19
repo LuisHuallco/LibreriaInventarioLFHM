027c1b78c66acb07027481d5fca043b7c7928d65
LuisHuallco <luishualco2005@gmail.com>
2025-06-19 17:29:48 -0500

    Add files via upload

---
+++ Aplicacion/InventarioService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario_Libreria.Aplicacion
+{
+    internal class InventarioService
+    {
+    }
+}
